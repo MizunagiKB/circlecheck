ce70db973f10c690f620cef1076593402d1b3abc
MizunagiKB <mizukb@LIVE.JP>
2015-02-01 18:03:27 +0900

    jsonの生成スクリプトを更新しました。

--- jsdata/util/make_jsdata.py
+++ jsdata/util/make_jsdata.py
@@ -1,62 +1,99 @@
 # -*- coding: utf-8 -*-
 ##
-import	sys
-import	string
-import	csv
-import	json
-import	collections
+import sys
+import string
+import csv
+import json
+import collections
 
+#
+COMITIA_A = u"ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"
+COMITIA_B = u"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめも"
+COMITIA_C = u"展"
 
-dictLayout	= {}
+dictLayout = {}
 
-oCReader = csv.reader(open(sys.argv[ 1 ], "rb"), delimiter = '\t')
+oCReader = csv.reader(open(sys.argv[1], "rb"), delimiter="\t")
 
 for tplRecord in oCReader:
 
-	dictRecord	= collections.OrderedDict()
+    dictRecord = {}
+    # collections.OrderedDict()
 
-	# CSV はこのルールで出力
-	for idx, kwd in ((0, "layout"), (1, "circle"), (2, "writer"), (3, "url")):
+    for idx, kwd in ((0, "layout"), (1, "circle"), (3, "writer"), (2, "url")):
+        s = tplRecord[idx].strip().decode("utf-8")
+        if(s != u"　" and len(s) > 0):
+            dictRecord[kwd] = s
+
+    strLayout_1 = tplRecord[2].strip()
+    strLayout_2 = tplRecord[3].strip().replace("/", ",")
+
+# dictRecord[ "url" ] = ""
+# dictRecord[ "layout" ] = strLayout_1 + strLayout_2
+
+    try:
+        strLB = dictRecord["layout"][0]
+    except:
+        # print dictRecord
+        pass
+
+    # print strLB
+
+    dictRecord = {"layout": dictRecord["layout"], "circle_list": dictRecord}
+    del dictRecord["circle_list"]["layout"]
+
+    if(strLB in dictLayout):
+        dictLayout[strLB].append(dictRecord)
+    else:
+        dictLayout[strLB] = [dictRecord]
 
 
-		if(tplRecord[0] == ""):
-			break
-
-		s	= tplRecord[ idx ].strip()
-		if( s != "　" and len( s ) > 0 ):
-			dictRecord[ kwd ]	= s
-
-	try:
-		strLB	= dictRecord[ "layout" ].decode( "utf-8" )[ 0 ]
-	except:
-		continue
-#	print strLB
+def check_key(v):
+    x = int(v["layout"][1:].split(",")[0])
+    return(x)
 
 
-	if( strLB in dictLayout ):
-		dictLayout[ strLB ].append( dictRecord )
-	else:
-		dictLayout[ strLB ]	= [ dictRecord ]
+nIndex = 0
 
-nIx = 0
-#for ss in u"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみ":
-#for ss in u"ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ":
-for ss in u"展":
+for k, listItem in dictLayout.items():
+    # print k
+    pass
 
-    k = ss
-    listItem = dictLayout[ss]
+# sys.exit()
 
-    """
-    for k, listItem in dictLayout.items():
-    """
+for k in COMITIA_C:
 
-    listBuffer	= []
+    listItem = dictLayout[k]
 
-    for o in sorted( listItem, key = lambda obj: obj[ "layout" ] ):
-        listBuffer.append( "\t" + json.dumps( o, ensure_ascii = False ) )
+    listBuffer = []
+    # print k, type(listItem)
 
-    print "\"%d\": [" % (nIx,)
-    #print "<!-- %s -->" % k.encode( "utf-8" )
-    print ",\n".join( listBuffer )
-    print "],"
-    nIx += 1
+    for o in sorted(listItem, key=lambda obj: obj["layout"]):
+        # listBuffer.append( "    " + json.dumps( o, ensure_ascii = False ) )
+        listBuffer.append(
+            "    {\"layout\": \"%s\",\n"
+            "        \"circle_list\": [\n"
+            "            %s\n"
+            "        ]\n"
+            "    }" % (
+                o["layout"],
+                json.dumps(o["circle_list"], ensure_ascii=False)
+            )
+        )
+
+    # print "<!-- %s -->" % k.encode( "utf-8" )
+    exportBuffer = ""
+    exportBuffer += "\"%d\" : [\n" % (nIndex,)
+    exportBuffer += ",\n".join(listBuffer)
+    exportBuffer += "\n"
+    exportBuffer += "],\n"
+
+    print exportBuffer.encode("utf-8")
+
+    nIndex += 1
+    # pass
+
+# for strLine in string.split( hFile.read(), "\n" ):
+
+    # strCircle, strWriter, strUrl, strLayout, x	= string.split( strLine, "\t" )
+    # print '{ "layout" : "%s", "circle" : "%s", "writer" : "%s", "url" : "%s" },' % ( strLayout, strCircle, strWriter, strUrl, )
